4f1cefe815e6b6d4aba1f75eecb5daff3f8fb13e
OxerProjects <122890182+OxerProjects@users.noreply.github.com>
2023-12-09 13:18:58 +0200

    Update alerts.js

--- javaScript/alerts.js
+++ javaScript/alerts.js
@@ -1,55 +1,76 @@
-//Alert
+// Alert
 var r = document.querySelector(':root');
-var backgroundColor
-const alertButton = document.querySelector('#alertButton')
+var backgroundColor;
+const alertButton = document.querySelector('#alertButton');
 
 function getBackground() {
     var rs = getComputedStyle(r);
-    backgroundColor = r.style.getPropertyValue('--background-color');
+    backgroundColor = rs.getPropertyValue('--background-color');
 }
 
 getBackground();
 
 function alertBackground() {
-  r.style.setProperty('--background-color', 'var(--alert-color)');
+    r.style.setProperty('--background-color', 'var(--alert-color)');
+}
+
+function areaAlert() {
+    document.querySelector('#areaBox').className = "areaBox-hide";
 }
 
 function deAlertBackground() {
-    r.style.setProperty('--background-color',`${backgroundColor}`);
+    r.style.setProperty('--background-color', `${backgroundColor}`);
 }
 
-//Button Activation
-let i = 0
+// Button Activation
+let i = 0;
 alertButton.addEventListener("click", () => {
     if (i == 0) {
-        alertBackground()
+        alertBackground();
         i = 1;
     } else {
-        deAlertBackground()
+        deAlertBackground();
         i = 0;
     }
-})
+});
 
-
-//Get Alerts
+// Get Alerts
+let ditectenRedAlert = 0
+let count = 0
 
 async function getAlerts() {
-    const response = await fetch("https://www.mako.co.il/Collab/amudanan/alerts.json");
-    const alertArray = await response.json();
-    if(alertArray.length > 0) {
-        console.log("נמצאה אזעקה ב:");
-        console.log(alertArray);
-        console.log(alertArray.data);
-        console.log(alertArray.cities);
-        alertArray.cities.array.forEach(element => {
-            if (element === "בית דגן") {
-                alertBackground()
-                document.querySelector('#areaBox').className = "areaBox"
-                startCountdown()
-            }
-        });
+    try {
+        const response = await fetch("https://www.mako.co.il/Collab/amudanan/alerts.json");
+        const alertArray = await response.json();
+        count++
+        if (alertArray.data.length > 0) {
+            console.log("נמצאה אזעקה ב:");
+            console.log(alertArray);
+            console.log(alertArray.data);
+            alertArray.data.forEach(element => {
+                if (element === "בית דגן") {
+                    if (ditectenRedAlert == 0) {
+                        count = 0
+                        startCountdown();
+                        alertBackground();
+                        ditectenRedAlert = 1
+                        document.querySelector('#areaBox').className = "areaBox";
+                    }
+                    if (count == 15) {
+                        areaAlert();
+                    }
+                    if (count == 600) {
+                        count = 0
+                        deAlertBackground();
+                        ditectenRedAlert = 0
+                    }
+                }
+            });
+        }
+        setTimeout(getAlerts, 1000);
+    } catch (err) {
+        console.error(err);
     }
-    setTimeout(getAlerts, 1000)
 }
 
-getAlerts()
+getAlerts();
